ae6b185f17217b2e0486fb8fada63bfdf6c5de75
JsonJiu <906025790@qq.com>
2025-12-08 19:48:04 +0800

    Delete 机械阀控 directory

--- 机械阀控/Inc/PC_xieyi_Ctrl.h
+++
@@ -1,5 +0,0 @@
-#ifndef __PC_XIEYI_CTRL_H__
-#define __PC_XIEYI_CTRL_H__
-#include "main.h"
-void PC_xieyijiexi(uint8_t zufuchua[],uint16_t lenth);
-#endif
--- 机械阀控/Inc/jiliang_xieyi_Ctrl.h
+++
@@ -1,9 +0,0 @@
-#ifndef __JILIANG_XIEYI_CTRL_H__
-#define __JILIANG_XIEYI_CTRL_H__
-#include "main.h"
-void JILIAN_TONGXIN_xieyijiexi(uint8_t zufuchua[],uint16_t lenth);
-void JILIAN_TONGXIN_xieyifasong(uint8_t zhuangtai,uint8_t zoushuidaxiao);
-void JILIAN_wushui_xieyi(void);
-void JILIAN_manshui_xieyi(void);
-void JILIAN_zoushui_xieyi(void);
-#endif
